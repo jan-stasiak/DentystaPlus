fee07c3eb90789d2a54e6986bb4256e7a2931f88
jan-stasiak <jan_stasiak@outlook.com>
2023-04-11 18:06:43 +0200

    feat: add main page and navbar

--- src/components/aboutus/Aboutus.tsx
+++ src/components/aboutus/Aboutus.tsx
@@ -1,9 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CSS from 'csstype';
+
+import Background from "../../assets/background.jpg";
+
+const backgroundStyle: CSS.Properties = {
+    backgroundImage: `url(${Background})`
+};
 
 export const Aboutus = () => {
     return (
-        <h1>Aboutus</h1>
+        <div className="h-screen w-screen flex flex-col justify-center items-center text-center bg-cover" style={backgroundStyle}>
+  <div className="bg-gray-500 bg-opacity-50 rounded-lg p-10 shadow-lg ">
+    <h1 className="text-5xl font-bold text-white mb-8 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+      Program, który polubisz od pierwszego kliknięcia
+    </h1>
+    <div className="text-lg text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+      <p>
+        Nasza platforma to gwarancja bezpieczeństwa i poufności Twoich danych medycznych.
+      </p>
+      <p>
+        Oferujemy elektroniczną historię leczenia oraz monitorowanie postępów Twojego zdrowia.
+      </p>
+    </div>
+  </div>
+</div>
+
     );
 };
 
--- src/containers/navbar/Navbar.tsx
+++ src/containers/navbar/Navbar.tsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import logo from '../../assets/logo.png'
+import { RiMenu3Fill, RiCloseLine, RiMenu3Line } from "react-icons/ri";
 
-export const Navbar = () => {
+interface MenuProps {
+  onClose: () => void;
+}
+
+
+const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    const handleMenuClose = () => {
+        setToggleMenu(false);
+    }
+
     return (
-        <h1>Navbar</h1>
+        <div className="flex justify-between items-center px-6 py-2">
+            <div className="flex justify-start items-center">
+                <div className="mr-6">
+                    <img src={logo} alt="logo" className="h-8" />
+                </div>
+                <div className="text-white upp text-xl my-2">
+                    <p>Dentysta+</p>
+                </div>
+            </div>
+            <div className="flex justify-end items-center">
+                <p className="mr-4 text-white capitalize"><Link to="/login">Login in</Link></p>
+                <button type="button" className="px-4 py-2 text-white font-medium bg-purple-600 rounded-full sm:rounded-lg">
+                    <Link to="/signup">Sign Up</Link>
+                </button>
+                <div className="ml-4 lg:hidden">
+                    {toggleMenu
+                        ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+                        : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+                    }
+                </div>
+            </div>
+        </div>
     );
-};
+}
+
 
 export default Navbar;
